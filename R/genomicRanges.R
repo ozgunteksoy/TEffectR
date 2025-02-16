f5a0b116afd8340e1d237c05c780658979fdeb4d
OZGUN <113980662+ozgunteksoy@users.noreply.github.com>
2025-02-16 18:22:37 +0300

    Update genomicRanges.R
    - Gereksiz c() Kullanımı Kaldırıldı
    c(df$start[indexP] - length) yerine df$start[indexP] - length şeklinde sadeleştirildi.
    
    - if-else Blokları Daha Temiz Hale Getirildi
    isWithGeneBody == FALSE yerine doğrudan if (!isWithGeneBody) kullanıldı.
    
     Kod Tekrarları Azaltıldı
    dfnew <- df satırı ilk başta tanımlandı, böylece dftemp gibi ekstra değişkenler kullanılmadı.
    
    - Mantıksal Koşullar Daha Okunur Hale Getirildi
    toFindOverlaps() içinde ignore.strand = TRUE doğrudan değişken üzerinden belirlenerek gereksiz if yapıları ortadan kaldırıldı.
    
    - Daha Temiz ve Performanslı Hale Getirildi
    mcols(gr) atamalarında gereksiz values(gr) kullanımı yerine mcols(gr) kullanıldı.

--- R/genomicRanges.R
+++ R/genomicRanges.R
@@ -1,94 +1,75 @@
-
-getUpstream <- function(df,length, isWithGeneBody){ # to get genome interval of up stream of given genome
-
-  dftemp <- df
+getUpstream <- function(df, length, isWithGeneBody) {
   dfnew <- df
 
-  indexP <- dftemp$strand == "+"
-  indexN <- dftemp$strand == "-"
+  indexP <- df$strand == "+"
+  indexN <- df$strand == "-"
 
-  dfnew$start[indexP] <- c(dftemp$start[indexP] - length)
-  dfnew$end[indexN] <- c(dftemp$end[indexN] + length)
+  dfnew$start[indexP] <- df$start[indexP] - length
+  dfnew$end[indexN] <- df$end[indexN] + length
 
-  if(isWithGeneBody == FALSE)
-  {
-    dfnew$end[indexP] <- c(dftemp$start[indexP])
-    dfnew$start[indexN] <- c(dftemp$end[indexN] )
+  if (!isWithGeneBody) {
+    dfnew$end[indexP] <- df$start[indexP]
+    dfnew$start[indexN] <- df$end[indexN]
   }
 
   return(dfnew)
 }
 
-getDownstream <- function(df,length,isWithGeneBody){ # to get genome interval of down stream of given genome
-  dftemp <- df
+getDownstream <- function(df, length, isWithGeneBody) {
   dfnew <- df
-  indexP <- dftemp$strand == "+"
-  indexN <- dftemp$strand == "-"
-  dfnew$start[indexN] <- dftemp$start[indexN] - length
-  dfnew$end[indexP] <- dftemp$end[indexP] + length
-  if(isWithGeneBody == FALSE)
-  {
-    dfnew$end[indexN] <- dftemp$start[indexN]
-    dfnew$start[indexP] <- dftemp$end[indexP]
+
+  indexP <- df$strand == "+"
+  indexN <- df$strand == "-"
+
+  dfnew$start[indexN] <- df$start[indexN] - length
+  dfnew$end[indexP] <- df$end[indexP] + length
+
+  if (!isWithGeneBody) {
+    dfnew$end[indexN] <- df$start[indexN]
+    dfnew$start[indexP] <- df$end[indexP]
   }
+
   return(dfnew)
 }
 
-getDownAndUpStream <- function(df,len_up,len_down){ # to get genome interval of up and down stream of given genome with its
-  dftemp <- df
+getDownAndUpStream <- function(df, len_up, len_down) {
   dfnew <- df
 
-  indexP <- dftemp$strand == "+"
-  indexN <- dftemp$strand == "-"
+  indexP <- df$strand == "+"
+  indexN <- df$strand == "-"
 
-  dfnew$start[indexP] <- dftemp$start[indexP]-len_up
-  dfnew$end[indexP] <- dftemp$end[indexP]+len_down
+  dfnew$start[indexP] <- df$start[indexP] - len_up
+  dfnew$end[indexP] <- df$end[indexP] + len_down
 
-  dfnew$start[indexN] <- dftemp$start[indexN]-len_down
-  dfnew$end[indexN] <- dftemp$end[indexN]+len_up
+  dfnew$start[indexN] <- df$start[indexN] - len_down
+  dfnew$end[indexN] <- df$end[indexN] + len_up
 
   return(dfnew)
 }
 
-
-makeGRangeObj <- function(df){  # to make grange object to analysis genome arithmetics well
-  gr <- with(df, GenomicRanges::GRanges(chr, IRanges(start, end), strand = strand))
-  values(gr)<-df[,5:length(df)]
-  values(gr)<-cbind(values(gr),df[2:3])
+makeGRangeObj <- function(df) {
+  gr <- GenomicRanges::GRanges(df$chr, IRanges(df$start, df$end), strand = df$strand)
+  mcols(gr) <- df[, 5:ncol(df)]
+  mcols(gr) <- cbind(mcols(gr), df[, 2:3])
   return(gr)
 }
 
-makeGrObj_Unstrand <- function(df){  #within strandness
-  gr <- with(df, GenomicRanges::GRanges(chr, IRanges(start, end), strand = "*"))
-  values(gr)<-df[,5:length(df)]
-  values(gr)<-cbind(values(gr),df[2:3])
+makeGrObj_Unstrand <- function(df) {
+  gr <- GenomicRanges::GRanges(df$chr, IRanges(df$start, df$end), strand = "*")
+  mcols(gr) <- df[, 5:ncol(df)]
+  mcols(gr) <- cbind(mcols(gr), df[, 2:3])
   return(gr)
 }
 
-toFindOverlaps<-function(gr_repeats,gr_genome,strand){  #to get overlap in general function
-  # Find overlaps
-  names(values(gr_genome))[3:4]<-c("gStart","gEnd")
-  names(values(gr_repeats))[4:5]<-c("rStart","rEnd")
-  if(strand=="same"){
-    m <- GenomicRanges::findOverlaps(gr_genome, gr_repeats)
-    gr_genome.matched <- gr_genome[queryHits(m)]
-    # Add the metadata from gr2
-    mcols(gr_genome.matched) <- cbind.data.frame(
-      mcols(gr_genome.matched),
-      mcols(gr_repeats[subjectHits(m)]))
+toFindOverlaps <- function(gr_repeats, gr_genome, strand) {
+  names(mcols(gr_genome))[3:4] <- c("gStart", "gEnd")
+  names(mcols(gr_repeats))[4:5] <- c("rStart", "rEnd")
 
-    return(gr_genome.matched)
-  }else if (strand=="strandness"){
-    m <- GenomicRanges::findOverlaps(gr_genome, gr_repeats,ignore.strand=TRUE)
-    gr_genome.matched <- gr_genome[queryHits(m)]
-    # Add the metadata from gr2
-    mcols(gr_genome.matched) <- cbind.data.frame(
-      mcols(gr_genome.matched),
-      mcols(gr_repeats[subjectHits(m)]))
+  ignore_strand <- strand != "same"
+  m <- GenomicRanges::findOverlaps(gr_genome, gr_repeats, ignore.strand = ignore_strand)
 
-    return(gr_genome.matched)
-  }
+  gr_genome.matched <- gr_genome[queryHits(m)]
+  mcols(gr_genome.matched) <- cbind(mcols(gr_genome.matched), mcols(gr_repeats[subjectHits(m)]))
 
-
+  return(gr_genome.matched)
 }
-
