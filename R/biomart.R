15506948c03497e50a06343227d770c60da685c5
OZGUN <113980662+ozgunteksoy@users.noreply.github.com>
2025-02-16 18:17:22 +0300

    Update biomart.R
    -Kod Tekrarı Azaltıldı
    get_ensembl_data() adlı tek bir fonksiyon yazarak filterTranscriptID, filterGeneID vb. fonksiyonlardaki tekrar eden işlemleri tek noktaya topladım.
    
    -Kod Daha Okunaklı Hale Getirildi
    data <- data[c(3,4,5,6,1,2)] gibi dizilimler daha açıklayıcı hale getirildi.
    data$strand dönüşümleri sadeleştirildi.
    
    - Hata Yönetimi ve Koşullar Daha İyi Hale Getirildi
    if (length(values) == 0) return(NULL) ile gereksiz işlemleri önledim.
    if (is.null(ensembl)) return(NULL) ile returnEnsembl() çağrısından dönen değerin olup olmadığı kontrol edildi.
    if (nrow(data) == 0) return(NULL) ile gereksiz dönüşler engellendi.
    
    - returnEnsembl() Fonksiyonu Optimize Edildi
    Daha temiz ve genişletilebilir olması için list yapısı kullanıldı.
    message("Not found assembly") eklendi, böylece kullanıcıya bilgilendirme yapılır.
    
    - %||% Operatörü Kullanıldı
    GRCh değeri sadece gerekli olduğunda fonksiyona veriliyor.
    GRCh = assembly_info$GRCh %||% NULL şeklinde kullanılarak opsiyonel hale getirildi.

--- R/biomart.R
+++ R/biomart.R
@@ -1,205 +1,112 @@
+get_ensembl_data <- function(attributes, filter_type, values, assembly, URL) {
+  if (length(values) == 0) return(NULL)
+  
+  ensembl <- returnEnsembl(assembly, URL)
+  if (is.null(ensembl)) return(NULL)
+  
+  data <- biomaRt::getBM(attributes = attributes, filters = filter_type, values = values, mart = ensembl, verbose = FALSE)
+  if (nrow(data) == 0) return(NULL)
 
-filterTranscriptID<-function(transcript_ids,assembly,URL){
-  if(length(transcript_ids)!=0){
-    ensembl = returnEnsembl(assembly,URL)
-    if(!is.null(ensembl)){
-      data <- biomaRt::getBM(attributes = c("ensembl_gene_id","external_gene_name",
-                                   "chromosome_name", "start_position", "end_position",
-                                   "strand"),
+  names(data) <- c("geneID", "geneName", "chr", "start", "end", "strand")
+  data <- data[c("chr", "start", "end", "strand", "geneID", "geneName")]
 
-                    filters = c("ensembl_transcript_id"), #listFilters(ensembl)
-                    values = transcript_ids,
-                    mart=ensembl,
-                    verbose = FALSE)
+  data$strand[data$strand == "-1"] <- "-"
+  data$strand[data$strand == "1"] <- "+"
+  
+  no_chr_prefix <- !stringr::str_detect(data$chr, "CHR")
+  data$chr[no_chr_prefix] <- paste0("chr", data$chr[no_chr_prefix])
 
-      names(data)<-c("geneID","geneName","chr","start","end","strand")
-      data <- data[c(3,4,5,6,1,2)]
-
-      if(length(data)>0){
-        hit<-stringr::str_detect(data$strand,"-1")
-        data$strand[hit]<-"-"
-
-        hit<-stringr::str_detect(data$strand,"1")
-        data$strand[hit]<-"+"
-
-        hit<-stringr::str_detect(data$chr,"CHR")==FALSE
-        data$chr[hit]<-paste("chr",data$chr[hit],sep = "")
-
-      }
-
-      return(data)
-    }
-
-  }else return(NULL)
-
-
+  return(data)
 }
 
-filterTranscriptID_V<-function(transcript_ids,assembly,URL){
-  if(length(transcript_ids)!=0){
-    ensembl = returnEnsembl(assembly,URL)
-    if(!is.null(ensembl)){
-      data <- biomaRt::getBM(attributes = c("ensembl_gene_id","external_gene_name",
-                                   "chromosome_name", "start_position", "end_position",
-                                   "strand"),
-
-                    filters = c("ensembl_transcript_id_version"), #listFilters(ensembl)
-                    values = transcript_ids,
-                    mart=ensembl,
-                    verbose = FALSE)
-
-      names(data)<-c("geneID","geneName","chr","start","end","strand")
-      data <- data[c(3,4,5,6,1,2)]
-
-      if(length(data)>0){
-        hit<-stringr::str_detect(data$strand,"-1")
-        data$strand[hit]<-"-"
-
-        hit<-stringr::str_detect(data$strand,"1")
-        data$strand[hit]<-"+"
-
-        hit<-stringr::str_detect(data$chr,"CHR")==FALSE
-        data$chr[hit]<-paste("chr",data$chr[hit],sep = "")
-      }
-
-      return(data)
-    }
-
-  }else return(NULL)
-
-
+filterTranscriptID <- function(transcript_ids, assembly, URL) {
+  get_ensembl_data(
+    attributes = c("ensembl_gene_id", "external_gene_name", "chromosome_name", 
+                   "start_position", "end_position", "strand"),
+    filter_type = "ensembl_transcript_id",
+    values = transcript_ids,
+    assembly = assembly,
+    URL = URL
+  )
 }
 
-
-filterGeneName<-function(genes,assembly,URL){
-  if(length(genes)!=0){
-    ensembl = returnEnsembl(assembly,URL)
-    gene_ids <- biomaRt::getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"), filters = c("hgnc_symbol"), values = genes, mart = ensembl, verbose = T)
-    if(!is.null(ensembl) & !is.null(gene_ids)){
-      data <- getBM(attributes = c("ensembl_gene_id","external_gene_name",
-                                   "chromosome_name", "start_position", "end_position",
-                                   "strand"),
-
-                    filters = c("ensembl_gene_id"), #listFilters(ensembl)
-                    values = gene_ids$ensembl_gene_id,
-                    mart=ensembl,
-                    verbose = FALSE)
-
-      names(data)<-c("geneID","geneName","chr","start","end","strand")
-      data <- data[c(3,4,5,6,1,2)]
-
-      if(length(data)>0){
-          hit<-stringr::str_detect(data$strand,"-1")
-          data$strand[hit]<-"-"
-
-          hit<-stringr::str_detect(data$strand,"1")
-          data$strand[hit]<-"+"
-
-          hit<-stringr::str_detect(data$chr,"CHR")==FALSE
-          data$chr[hit]<-paste("chr",data$chr[hit],sep = "")
-      }
-
-
-      return(data)
-    }
-
-  }else return(NULL)
-
+filterTranscriptID_V <- function(transcript_ids, assembly, URL) {
+  get_ensembl_data(
+    attributes = c("ensembl_gene_id", "external_gene_name", "chromosome_name", 
+                   "start_position", "end_position", "strand"),
+    filter_type = "ensembl_transcript_id_version",
+    values = transcript_ids,
+    assembly = assembly,
+    URL = URL
+  )
 }
 
+filterGeneName <- function(genes, assembly, URL) {
+  if (length(genes) == 0) return(NULL)
+  
+  ensembl <- returnEnsembl(assembly, URL)
+  if (is.null(ensembl)) return(NULL)
+  
+  gene_ids <- biomaRt::getBM(attributes = c("ensembl_gene_id", "hgnc_symbol"), 
+                             filters = "hgnc_symbol", values = genes, mart = ensembl, verbose = FALSE)
 
-filterGeneID_V<-function(gene_ids,assembly,URL){
-  if(length(gene_ids)!=0){
-    ensembl = returnEnsembl(assembly,URL)
-    if(!is.null(ensembl) & !is.null(gene_ids)){
-      data <- biomaRt::getBM(attributes = c("ensembl_gene_id_version","external_gene_name",
-                                   "chromosome_name", "start_position", "end_position",
-                                   "strand"),
-
-                    filters = c("ensembl_gene_id_version"), # listFilters(ensembl)
-                    values = gene_ids,
-                    mart=ensembl,
-                    verbose = FALSE)
-
-      names(data)<-c("geneID","geneName","chr","start","end","strand")
-      data <- data[c(3,4,5,6,1,2)]
-
-      if(length(data)>0){
-        hit<-stringr::str_detect(data$strand,"-1")
-        data$strand[hit]<-"-"
-
-        hit<-stringr::str_detect(data$strand,"1")
-        data$strand[hit]<-"+"
-
-        hit<-stringr::str_detect(data$chr,"CHR")==FALSE
-        data$chr[hit]<-paste("chr",data$chr[hit],sep = "")
-      }
-
-      return(data)
-    }
-
-  }else return(NULL)
-
+  if (nrow(gene_ids) == 0) return(NULL)
+  
+  get_ensembl_data(
+    attributes = c("ensembl_gene_id", "external_gene_name", "chromosome_name", 
+                   "start_position", "end_position", "strand"),
+    filter_type = "ensembl_gene_id",
+    values = gene_ids$ensembl_gene_id,
+    assembly = assembly,
+    URL = URL
+  )
 }
 
-
-filterGeneID<-function(gene_ids,assembly,URL){
-  if(length(gene_ids)!=0){
-    ensembl = returnEnsembl(assembly,URL)
-    if(!is.null(ensembl) & !is.null(gene_ids)){
-      data <- biomaRt::getBM(attributes = c("ensembl_gene_id","external_gene_name",
-                                   "chromosome_name", "start_position", "end_position",
-                                   "strand"),
-
-                    filters = c("ensembl_gene_id"), # listFilters(ensembl)
-                    values = gene_ids,
-                    mart=ensembl,
-                    verbose = FALSE)
-
-      names(data)<-c("geneID","geneName","chr","start","end","strand")
-      data <- data[c(3,4,5,6,1,2)]
-
-      if(length(data)>0){
-        hit<-stringr::str_detect(data$strand,"-1")
-        data$strand[hit]<-"-"
-
-        hit<-stringr::str_detect(data$strand,"1")
-        data$strand[hit]<-"+"
-
-        hit<-stringr::str_detect(data$chr,"CHR")==FALSE
-        data$chr[hit]<-paste("chr",data$chr[hit],sep = "")
-
-      }
-
-      return(data)
-    }
-
-  }else return(NULL)
-
+filterGeneID_V <- function(gene_ids, assembly, URL) {
+  get_ensembl_data(
+    attributes = c("ensembl_gene_id_version", "external_gene_name", "chromosome_name", 
+                   "start_position", "end_position", "strand"),
+    filter_type = "ensembl_gene_id_version",
+    values = gene_ids,
+    assembly = assembly,
+    URL = URL
+  )
 }
 
-
-
-
-returnEnsembl<-function(assembly,URL){
-  if(assembly=="hg19" || assembly=="Grch37"){
-    ensembl = biomaRt::useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh = 37, host = URL, verbose = TRUE)
-    return(ensembl)
-  }else if(assembly=="hg38" || assembly=="Grch38"){
-    ensembl = biomaRt::useEnsembl(biomart="ensembl", dataset="hsapiens_gene_ensembl", GRCh = 38, host = URL, verbose = TRUE)
-    return(ensembl)
-  }else if(y=="mm9" || y =="Grcm37"){
-    ensembl = biomaRt::useEnsembl(biomart="ensembl", dataset="mmusculus_gene_ensembl",  host = URL, verbose = TRUE)
-    return(ensembl)
-  }else if(y=="mm10" || y =="Grcm38"){
-    ensembl = biomaRt::useEnsembl(biomart="ensembl", dataset="mmusculus_gene_ensembl",  host = URL, verbose = TRUE)
-    return(ensembl)
-  }else{
-    print("not found assembly")
-    return(NULL)}
+filterGeneID <- function(gene_ids, assembly, URL) {
+  get_ensembl_data(
+    attributes = c("ensembl_gene_id", "external_gene_name", "chromosome_name", 
+                   "start_position", "end_position", "strand"),
+    filter_type = "ensembl_gene_id",
+    values = gene_ids,
+    assembly = assembly,
+    URL = URL
+  )
 }
 
+returnEnsembl <- function(assembly, URL) {
+  assembly_map <- list(
+    hg19 = list(dataset = "hsapiens_gene_ensembl", GRCh = 37),
+    Grch37 = list(dataset = "hsapiens_gene_ensembl", GRCh = 37),
+    hg38 = list(dataset = "hsapiens_gene_ensembl", GRCh = 38),
+    Grch38 = list(dataset = "hsapiens_gene_ensembl", GRCh = 38),
+    mm9 = list(dataset = "mmusculus_gene_ensembl"),
+    Grcm37 = list(dataset = "mmusculus_gene_ensembl"),
+    mm10 = list(dataset = "mmusculus_gene_ensembl"),
+    Grcm38 = list(dataset = "mmusculus_gene_ensembl")
+  )
 
+  assembly_info <- assembly_map[[assembly]]
+  if (is.null(assembly_info)) {
+    message("Not found assembly")
+    return(NULL)
+  }
 
-
-
+  return(biomaRt::useEnsembl(
+    biomart = "ensembl", 
+    dataset = assembly_info$dataset, 
+    GRCh = assembly_info$GRCh %||% NULL, 
+    host = URL, 
+    verbose = TRUE
+  ))
+}
